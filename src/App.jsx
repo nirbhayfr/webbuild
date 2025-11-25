11634ef5eb07fea21f4f5f68e3ab3b01a627bc08
Nirbhay Chauhan <nirbhaychauhan0306@gmail.com>
2025-11-25 10:49:06 +0530

    //

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,21 @@ import Servicespage from "../components/services/Servicespage";
 import Projectpage from "../components/projects/Projectpage";
 import Contactpage from "../components/contact/Contactpage";
 
-
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Homepage />} />
-		    <Route path="home" element={<Homepage />} />
-          <Route path="about" element={<Aboutpage />} />
-		   <Route path="services" element={<Servicespage />} />
-		    <Route path="projects" element={<Projectpage />} />
-			 <Route path="contact" element={<Contactpage />} />
-
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<AppLayout />}>
+					<Route index element={<Homepage />} />
+					{/* <Route path="home" element={<Homepage />} /> */}
+					<Route path="/about" element={<Aboutpage />} />
+					<Route path="/services" element={<Servicespage />} />
+					<Route path="/projects" element={<Projectpage />} />
+					<Route path="/contact" element={<Contactpage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
